1f1abe1a42149fdc6fcb35af97e250a35965bcd8
DR Dev <dromeroldev@gmail.com>
2025-01-19 02:59:19 -0500

    Archivos del proyecto, primer commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1 align="center">Foro Hub API</h1>
+
+
+## Proyecto foro Hub
+La presente API fue creada en base a los pAtrones de Diseño API RESTFUL para la creaciónde un foro de un curso de tecnología.
+
+En el se pueden hacer difenrentes acciones como mirar los tópicos actuales que tiene la plataforma.
+
+El presente proyecto usa JAVA 17 + Spring y una base de datos MYSQL para almacenar toda la información.
+
+
+## Agradecimientos
+Agradezco al Programa ONE de [Alura Latam](https://www.linkedin.com/company/alura-latam/) y
+[Oracle](https://www.linkedin.com/company/oracle/) ya que se ellos proveeron datos y material apra poder realizar el presente proyecto.
+
---
+++ alura_ForoHub/src/main/java/com/forohub/foro_api/model/Usuario.java
@@ -0,0 +1,84 @@
+package com.forohub.foro_api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Nombre es obligatorio")
+    private String nombre;
+
+    @NotNull(message = "El campo email no puede estar vacío")
+    @Email(message = "Debe ser una dirección de correo electrónico válida")
+    private String email;
+
+    @NotNull(message = "El campo clave no puede estar vacío")
+    @Size(min = 6, max = 300, message = "La clave debe tener mínimo 6 caracteres")
+    private String clave;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return clave;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
